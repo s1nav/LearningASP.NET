133c97113058e4bc90e4a64cfd186f26170941d5
Andrey Sinitsyn <34270798+s1nav@users.noreply.github.com>
2020-11-10 21:59:55 +0300

    Add project files.

---
+++ FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+    }
+}
